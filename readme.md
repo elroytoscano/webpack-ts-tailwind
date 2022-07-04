f1716a482c32a1c66e63a0fda9857a564f9c09d1
elroytoscano <andeltos@gmail.com>
2022-07-04 21:50:10 +0530

    initial commit

---
+++ readme.md
@@ -0,0 +1,13 @@
+# HTML, Vanilla TS and Webpack setup
+
+## [Webpack Tutorial by Brad Traversy](https://www.youtube.com/watch?v=IZGNcSuwBZs)
+
+### Features
+
+- Allows importing of TypeScript files
+- Allows **Hot reloading** of **HTML**, **SCSS** and other _assets_
+
+## Styling
+
+- Styles belong to coming-soon-base-apparell-landing-page project
+- Folder structure to be continued for further projects
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src/index.ts'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    clean: true,
+    assetModuleFilename: '[name][ext]',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'dist'),
+    },
+    port: 8000,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['.ts', '.js', ''],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        include: path.resolve(__dirname, 'src'),
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|svg|jpeg|jpg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        include: [path.resolve(__dirname, 'src')],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/template.html',
+    }),
+  ],
+};
